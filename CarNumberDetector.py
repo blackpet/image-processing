13f2468aa5d97a884c53e601915204e71079e274
blackpet <blackpet@gmail.com>
2020-03-20 13:58:25 +0900

    coding.... 중

--- CarNumberDetector.py
+++ CarNumberDetector.py
@@ -3,15 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 
+
 def find_chars(contour_list):
     matched_result_idx = []
+    matched_contours_idx = []
 
     for d1 in contour_list:
-        matched_contours_idx = []
-
         for d2 in contour_list:
-
-            # 같은 Contour은 검사할 필요가 없다!
             if d1['idx'] == d2['idx']:
                 continue
 
@@ -20,24 +18,18 @@ def find_chars(contour_list):
 
             diagonal_length1 = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
 
-            # 대각선 거리는?
-            distance = np.linalg.norm(
-                np.array([d1['cx'], d1['cy']])
-                - np.array([d2['cx'], d2['cy']]))
-
+            distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
             if dx == 0:
                 angle_diff = 90
             else:
                 angle_diff = np.degrees(np.arctan(dy / dx))
-
             area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
             width_diff = abs(d1['w'] - d2['w']) / d1['w']
             height_diff = abs(d1['h'] - d2['h']) / d1['h']
 
-
             if distance < diagonal_length1 * MAX_DIAG_MULTIPLYER \
-            and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
-            and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
+                    and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
+                    and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
                 matched_contours_idx.append(d2['idx'])
 
         # append this contour
@@ -110,7 +102,8 @@ contours, _ = cv2.findContours(
 temp_result = np.zeros((height, width, channel), dtype=np.uint8)
 
 # cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
-
+#
+# cv2.imshow('Contours0', temp_result)
 
 ### Prepare Data
 contours_dict = []
@@ -130,10 +123,11 @@ for ct in contours:
         'cy': y + (h / 2)
     })
 
+cv2.imshow('Contours0', temp_result)
 
 ### Select Candidates by Char Size
 MIN_AREA = 80
-MIN_WIDTH, MIN_HEIGHT = 10, 30
+MIN_WIDTH, MIN_HEIGHT = 1, 30
 MAX_WIDTH, MAX_HEIGHT = 30, 90
 MIN_RATIO, MAX_RATIO = 0.25, 1.0
 
@@ -147,9 +141,9 @@ for d in contours_dict:
     if area > MIN_AREA \
         and MIN_WIDTH < d['w'] < MAX_WIDTH and MIN_HEIGHT < d['h'] < MAX_HEIGHT \
         and MIN_RATIO < ratio < MAX_RATIO:
-        print(d)
         d['idx'] = cnt
-        ++cnt
+        print(d)
+        cnt += 1
         possible_contours.append(d)
 
 # visualize possible contours
@@ -185,10 +179,10 @@ for r in matched_result:
 
 
 
-cv2.imshow('Car Number', img_origin)
+# cv2.imshow('Car Number', img_origin)
 # cv2.imshow('Gray', img_gray)
 # cv2.imshow('Gray + TopHat + BlackHat', img_gray2)
-cv2.imshow('Blurred', img_blurred)
+# cv2.imshow('Blurred', img_blurred)
 cv2.imshow('Threshold', img_thresh)
 cv2.imshow('Contours2', temp_result)
 
